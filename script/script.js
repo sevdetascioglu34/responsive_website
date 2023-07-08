4036bcf17d4854e680def9d838e82f654e11ba7c
Sevde Taşcıoğlu <131602297+sevdetascioglu34@users.noreply.github.com>
2023-07-08 14:40:30 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,10 @@
+const searchForm=document.querySelector(".search-form");
+const searchBtn=document.querySelector("#search-btn");
+
+searchBtn.addEventListener("click",function(){
+    searchForm.classList.toggle("active");
+    document.addEventListener("click",function(e)
+    {
+
+    })
+})
